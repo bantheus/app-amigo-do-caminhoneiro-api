8ebf3954a60f004bf7614d22d11c958dc6f01cc3
Matheus <matheuschmidt.b@gmail.com>
2025-05-23 17:05:33 -0300

    chore: add index file for drizzle schema exports

---
+++ src/infrastructure/database/drizzle/schema/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export * from './abastecimento';
+export * from './despesa';
+export * from './documentacao-veiculo';
+export * from './manutencao';
+export * from './parada';
+export * from './usuario';
+export * from './veiculo';
+export * from './viagem';
+
