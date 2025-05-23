70e20ec50648f661b47ab9f0d810166e97a00d5d
Matheus <matheuschmidt.b@gmail.com>
2025-05-23 17:31:47 -0300

    feat: add JWT authentication strategy

---
+++ src/aplication/interfaces/payload.interface.ts
@@ -0,0 +1,6 @@
+export interface Payload {
+  email: string;
+  sub: string;
+  iat?: number;
+  exp?: number;
+}
---
+++ src/infrastructure/auth/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import type { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import type { Payload } from 'src/aplication/interfaces/payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    const jwtSecret = configService.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET não encontrada nas variáveis de ambiente');
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  validate(payload: Payload) {
+    return { userId: payload.sub, email: payload.email };
+  }
+}
